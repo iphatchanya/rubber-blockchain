71154129dd2ddcc225f220ac6400ee6d73e14695
iphatchanya <phatchanya.c@ku.th>
2020-03-19 15:31:52 +0700

    delete internalMessage

--- workflows/src/main/kotlin/com/template/flows/QueryByAccountUUID.kt
+++ workflows/src/main/kotlin/com/template/flows/QueryByAccountUUID.kt
@@ -3,7 +3,6 @@ package com.template.flows
 import net.corda.core.flows.*
 import co.paralleluniverse.fibers.Suspendable
 import com.r3.corda.lib.accounts.workflows.accountService
-import com.template.states.InternalMessageState
 import com.template.states.TemplateState
 import net.corda.core.flows.FlowLogic
 import net.corda.core.flows.StartableByRPC
@@ -24,11 +23,6 @@ class ViewInboxByAccount(
         val criteria = QueryCriteria.VaultQueryCriteria(
                 externalIds = listOf(myAccount.identifier.id)
         )
-        val InternalMessages = serviceHub.vaultService.queryBy(
-                contractStateType = InternalMessageState::class.java,
-                criteria = criteria
-        ).states.map { "\n" + "Internal Message: " + it.state.data.task }
-
         val invoices = serviceHub.vaultService.queryBy(
                 contractStateType = TemplateState::class.java,
                 criteria = criteria
