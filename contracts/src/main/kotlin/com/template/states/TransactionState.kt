25e9124bb8d3ec9783c9a85f5d8c976e0dc9b4fb
iphatchanya <phatchanya.c@ku.th>
2020-04-09 01:53:25 +0700

    edit viewMyAccount4

--- contracts/src/main/kotlin/com/template/states/TransactionState.kt
+++ contracts/src/main/kotlin/com/template/states/TransactionState.kt
@@ -7,12 +7,12 @@ import net.corda.core.identity.AnonymousParty
 import java.util.*
 
 @BelongsToContract(TransactionContract::class)
-class TransactionState(val source: AnonymousParty,
+class TransactionState(val sender: AnonymousParty,
                             val rubberType: String,
                             val volume : Int,
                             val price : Int,
-                            val destination: AnonymousParty,
+                            val recipient: AnonymousParty,
                             val invoiceID: UUID) : ContractState {
-    override val participants: List<AbstractParty> get() = listOfNotNull(destination,source).map { it }
+    override val participants: List<AbstractParty> get() = listOfNotNull(recipient,sender).map { it }
 //    override val participants: List<AbstractParty> get() = listOf(destination,source).map{ it }
 }
--- workflows/src/main/kotlin/com/template/flows/ViewInboxByAccount.kt
+++ workflows/src/main/kotlin/com/template/flows/ViewInboxByAccount.kt
@@ -3,6 +3,7 @@ package com.template.flows
 import net.corda.core.flows.*
 import co.paralleluniverse.fibers.Suspendable
 import com.r3.corda.lib.accounts.workflows.accountService
+import com.r3.corda.lib.accounts.workflows.flows.RequestKeyForAccount
 import com.template.states.TransactionState
 import net.corda.core.flows.FlowLogic
 import net.corda.core.flows.StartableByRPC
@@ -27,13 +28,14 @@ class ViewInboxByAccount(
                 contractStateType = TransactionState::class.java,
                 criteria = criteria
         ).states.map {
-            "\n" + "State data = " + it.state.data +
+            "\n" + "State data = " + it.state.data.participants +
+            "\n" + "SubFlowSender = " + accountService.accountInfo(it.state.data.sender.toString()).single().state.data +
+            "\n" + "SubFlowRecipient = " + accountService.accountInfo(it.state.data.recipient.toString()).single().state.data +
             "\n" +" Invoice State : Invoice ID = " + it.state.data.invoiceID +
-                ", Source = " + it.state.data.source.toString() + ", Rubber type = " + it.state.data.rubberType +
+                ", Source = " + it.state.data.sender + ", Rubber type = " + it.state.data.rubberType +
                 ", Volume = " + it.state.data.volume + ", Price = " + it.state.data.price +
-                ", Destination = " + it.state.data.destination.toString() +
+                ", Destination = " + it.state.data.recipient.toString()
 //            "\n" + "Invoice State : " + it.state.data
-            "\n" + "Notary name = " + it.state.notary.name.toString() + "\n" + "Account Name = " + accountName
 
 
         }
