d72389525119dbb65712567b634aec77ffbff1de
iphatchanya <phatchanya.c@ku.th>
2020-04-09 21:14:06 +0700

    edit viewMyAccount13

--- workflows/src/main/kotlin/com/template/flows/ViewInboxByAccount.kt
+++ workflows/src/main/kotlin/com/template/flows/ViewInboxByAccount.kt
@@ -29,9 +29,10 @@ class ViewInboxByAccount(
         ).states.map {
 //            "\n" +" Invoice State : " + it.state.data
             "\n" +" Invoice State : Invoice ID = " + it.state.data.invoiceID +
-                ", Source = " + accountService.accountInfo(it.state.data.source.toString()) + ", Rubber type = " + it.state.data.rubberType +
+                ", Source = " + accountService.accountInfo(it.state.data.source.toString()).single().state.data +
+                ", Rubber type = " + it.state.data.rubberType +
                 ", Volume = " + it.state.data.volume + ", Price = " + it.state.data.price +
-                ", Destination = " + accountService.accountInfo(it.state.data.destination.toString())
+                ", Destination = " + accountService.accountInfo(it.state.data.destination.toString()).single()
         }
 
         return invoices
